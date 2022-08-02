ee0bf8b02da1d996f8f698cceae4559224fd60d3
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-08-02 09:29:56 +0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,15 @@
+## Thực ra thì lập trình hướng đối tượng cũng không khó lắm đâu
+Chúng ta hãy cùng hình dung về các cô gái của chúng ta cho dễ nhé
+![hướng đối tượng](images/huongdoituong.png)
+## Tính đóng gói
+Các thông tin của các cô gái luôn là điều gì đó bí ẩn đối với chúng ta. Ai mà hiểu được con gái nghĩ gì chứ.
+![tính đóng gói](images/tinhdonggoi.png)
+## Tính trừu tượng
+Nếu bạn muốn biết thông tin gì đó về cô gái, chỉ khi họ cho bạn biết thì bạn mới biết thui á. Chứ ai mà cũng biết hết thì còn gì thú vị nữa.
+![tính trừu tượng](images/tinhtruutuong.png)
+## Tính kế thừa
+Con gái nói chung đều là phụ nữ cả, họ đều có các đặc điểm giống nhau, dịu dàng này, xinh đẹp này, nụ cười nựa :))))
+![tính kế thừa](images/tinhkethua.png)
+## Tính đa hình
+Nói thì giống nhau thế thôi, chứ thực ra họ cũng có 1 vài điểm khác nhau đấy nhé. Gái ngoan thì có thể thích nấu ăn nè, thích ngồi ngắm ny chơi game nè. Còn gái hư thì đi bar nè, mẹ có đánh vào mông cũng phải đi phượt nè.
+![tính đa hình](images/tinhdahinh.png)
---
+++ simple oop exmaple/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+public class PhuNu
+{
+    public PhuNu(string ht, int t, bool zin = true)
+    {
+        hoTen = ht;
+        conZin = zin;
+        tuoi = t;
+    }
+
+    public void TrangDiem()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine(hoTen + " đang trang điểm");
+    }
+    public string ChoiNhacCu(string nhacCu)
+    {
+        return nhacCu;
+    }
+
+    public virtual void TiengKeu()
+    {
+        Console.WriteLine(hoTen + " đang kêu A A A A A ....");
+    }
+    // tính trừu tượng, bên ngoài muốn truy cập thì không thể truy cập tùy ý
+    // mà phải thông qua method mà chúng ta cung cấp
+    public string ShowTen()
+    {
+        return hoTen;
+    }
+
+    public void VaZin()
+    {
+        if (tuoi < 25)
+        {
+            conZin = true;
+        }
+    }
+
+    public void PhaZin()
+    {
+        conZin = false;
+    }
+
+    public bool ShowHang()
+    {
+        return conZin;
+    }
+
+    // tính đóng gói, các giá trị không thể truy cập trực tiếp từ bên ngoài
+    protected string hoTen;
+    protected bool conZin;
+    protected int tuoi;
+}
+
+public class GaiNgoan : PhuNu
+{
+    // tính kế thừa, các class con sẽ sở hữu toàn bộ
+    // method và property của class cha
+    public GaiNgoan(string ht, int t, bool zin = true) : base(ht, t, zin)
+    { }
+    public void NoiTro()
+    {
+        Console.WriteLine(hoTen + " đang làm nội trợ");
+    }
+    // tính đa hình, mỗi class con sẽ có những hình thái khác nhau thay vì giống
+    // hoàn toàn với class cha
+    // ở đây gái ngoan sẽ có tiếng kêu 1 kiểu, gái hư sẽ có tiếng kêu 1 kiểu khác nhau
+    public override void TiengKeu()
+    {
+        Console.WriteLine(hoTen + " đang kêu O O O O O ....");
+    }
+}
+public class GaiHu : PhuNu
+{
+    public GaiHu(string ht, int t, bool zin = true) : base(ht, t, zin)
+    { }
+    public void DiBar()
+    {
+        Console.WriteLine(hoTen + " đang đi vô bar á");
+    }
+    public override void TiengKeu()
+    {
+        Console.WriteLine(hoTen + " đang kêu Uh Uh Uh Uh Uh ....");
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        PhuNu mariaOzawa = new GaiNgoan("mariaOzawa", 29, false);
+        mariaOzawa.PhaZin();
+        mariaOzawa.VaZin();
+        Console.WriteLine(mariaOzawa.ShowTen() + " " + $"{(mariaOzawa.ShowHang() ? "còn zin" : "đã mất zin")}");
+        mariaOzawa.TrangDiem();
+        //mariaOzawa.NoiTro();
+        mariaOzawa.TiengKeu();
+
+        Console.WriteLine("\n");
+
+        PhuNu riku = new GaiHu("riku", 20, false);
+        riku.PhaZin();
+        riku.VaZin();
+        Console.WriteLine(riku.ShowTen() + " " + $"{(riku.ShowHang() ? "còn zin" : "đã mất zin")}");
+        riku.TrangDiem();
+        //riku.DiBar();
+        riku.TiengKeu();
+    }
+}
